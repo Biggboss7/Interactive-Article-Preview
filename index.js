1cea220793ee56f66fe41a0f793ae6813595616e
Michael Tze <105411073+Biggboss7@users.noreply.github.com>
2022-07-23 12:20:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(".profile .share-btn").click(function () {
+  window.innerWidth > 740
+  ? ($(".pop-up").toggleClass("appear"))
+  : ($(".contact-detail").addClass("show"))
+    $(".contact-detail .share-btn").click(function () {
+    $(".contact-detail").removeClass("show");
+    });
+})
